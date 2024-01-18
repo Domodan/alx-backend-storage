d517a818bdabaacc852ff9df5c7f117c7980c507
root <ongomdaniel9@gmail.com>
2024-01-18 07:27:16 +0000

    written a SQL script that lists all bands with Glam rock as their main style, ranked by their longevity

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,14 +1,5 @@
--- This SQL script creates a trigger named 'reduce_quantity' that runs after an insert on the 'orders' table.
--- The trigger reduces the quantity of an item in the 'items' table by the number of items ordered in the new 'orders' row.
-
-DROP TRIGGER IF EXISTS reduce_quantity;
-DELIMITER $$
-CREATE TRIGGER reduce_quantity
-AFTER INSERT ON orders
-FOR EACH ROW
-BEGIN
-    UPDATE items
-        SET quantity = quantity - NEW.number
-        WHERE name = NEW.item_name;
-END $$
-DELIMITER ;
+-- This SQL script select band_name, and lifespan column which is difference
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
